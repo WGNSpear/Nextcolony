8b8c5cdf0dccee3f503959b13b124c891d4f76b7
WGNSpear <40544275+WGNSpear@users.noreply.github.com>
2019-04-30 21:47:25 +0000

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+    let pingembed = new Discord.RichEmbed()
+    .setTitle("Checking Ping... Please Wait")
+    .setDescription(" ")
+    .setColor("#0000ff")
+
+    let finalpingembed = new Discord.RichEmbed()
+    .setTitle(":ping_pong: Pong!")
+    .setDescription("Latency: " + Math.round(bot.ping.toLocaleString()) + "ms")
+    .setColor("#0000ff")
+
+    message.channel.send(pingembed).then(function(m) {
+    m.edit(finalpingembed)
+  });
+
+}
+// bot.ping.toLocaleString()
+
+module.exports.help = {
+  name: "ping"
+}
